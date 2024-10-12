0eb6a4bc5be24502f111fdadf51056b77024f3b6
Madalin Turluianu <turluianu.petre.madalin@gmail.com>
2024-10-12 21:13:03 +0300

    small ui adjustments

--- src/app/components/shared/edit-item/edit-item.component.ts
+++ src/app/components/shared/edit-item/edit-item.component.ts
@@ -1,17 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-edit-item',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, MatCardModule],
   templateUrl: './edit-item.component.html',
   styleUrl: './edit-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditItemComponent {
+  title = input.required<string>();
   prevLink = input<string | string[] | undefined>();
   nextLink = input<string | string[] | undefined>();
 }
